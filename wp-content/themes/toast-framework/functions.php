e8599cc61540cf20cdcd8e3699e83ff0e57605ca
Toast Design <inf0@toastdesign.nl>
2014-07-09 14:23:38 +0200

    better

--- wp-content/themes/toast-framework/functions.php
+++ wp-content/themes/toast-framework/functions.php
@@ -144,4 +144,64 @@ if ( ! function_exists('toast_paging_nav' ) ) {
     }
 }
 
+/* ==========================================================================
+7.0 - Registreer de widgets
+========================================================================== */
+if (! function_exists('toast_widget_init') ) {
+    function toast_widget_init() {
+        if ( function_exists('register_sidebar') ) {
+            register_sidebar(
+                array(
+                    'name' => __( 'Main Widget Area', 'toast' ),
+                    'id' => 'sidebar-1',
+                    'description' => __( 'Appears on posts and pages.', 'toast' ),
+                    'before_widget' => '<div id="%1$s" class="widget %2$s">',
+                    'after_widget' => '</div> <!-- end widget -->',
+                    'before_title' => '<h5 class="widget-title">',
+                    'after_title' => '</h5>',
+                )
+            );
+
+            register_sidebar(
+                array(
+                    'name' => __( 'Footer Widget Area', 'toast' ),
+                    'id' => 'sidebar-2',
+                    'description' => __( 'Appears on the footer.', 'toast' ),
+                    'before_widget' => '<div id="%1$s" class="widget col-sm-3 %2$s">',
+                    'after_widget' => '</div> <!-- end widget -->',
+                    'before_title' => '<h5 class="widget-title">',
+                    'after_title' => '</h5>',
+                )
+            );
+        }
+    }
+
+    add_action( 'widgets_init', 'toast_widget_init' );
+}
+
+/* ==========================================================================
+8.0 - Laden van scripts
+========================================================================== */
+if ( ! function_exists( 'toast_scripts' ) ) {
+    function toast_scripts() {
+        // Adds support for pages with threaded comments
+        if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+            wp_enqueue_script( 'comment-reply' );
+        }
+
+        // Register scripts
+        wp_register_script( 'bootstrap-js', 'http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js', array( 'jquery' ), false, true );
+        wp_register_script( 'toast-custom', SCRIPTS . '/scripts.js', array( 'jquery' ), false, true );
+
+        // Load the custom scripts
+        wp_enqueue_script( 'bootstrap-js' );
+        wp_enqueue_script( 'toast-custom' );
+
+        // Load the stylesheets
+        wp_enqueue_style( 'font-awesome', THEMEROOT . '/css/font-awesome.min.css' );
+        wp_enqueue_style( 'toast-master', THEMEROOT . '/css/master.css' );
+    }
+
+    add_action( 'wp_enqueue_scripts', 'toast_scripts' );
+}
 ?>
