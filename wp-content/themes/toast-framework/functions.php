76d7803b3089aac5300e259170337a51b1d20870
Toast Design <inf0@toastdesign.nl>
2014-07-11 06:57:48 +0200

    Keep on track

--- wp-content/themes/toast-framework/functions.php
+++ wp-content/themes/toast-framework/functions.php
@@ -179,29 +179,6 @@ if (! function_exists('toast_widget_init') ) {
     add_action( 'widgets_init', 'toast_widget_init' );
 }
 
-/* ==========================================================================
-8.0 - Laden van scripts
-========================================================================== */
-if ( ! function_exists( 'toast_scripts' ) ) {
-    function toast_scripts() {
-        // Adds support for pages with threaded comments
-        if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-            wp_enqueue_script( 'comment-reply' );
-        }
 
-        // Register scripts
-        wp_register_script( 'bootstrap-js', 'http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js', array( 'jquery' ), false, true );
-        wp_register_script( 'toast-custom', SCRIPTS . '/scripts.js', array( 'jquery' ), false, true );
 
-        // Load the custom scripts
-        wp_enqueue_script( 'bootstrap-js' );
-        wp_enqueue_script( 'toast-custom' );
-
-        // Load the stylesheets
-        wp_enqueue_style( 'font-awesome', THEMEROOT . '/css/font-awesome.min.css' );
-        wp_enqueue_style( 'toast-master', THEMEROOT . '/css/master.css' );
-    }
-
-    add_action( 'wp_enqueue_scripts', 'toast_scripts' );
-}
 ?>
